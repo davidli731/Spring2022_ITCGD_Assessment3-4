51c5574f4048f85b12e4baac81155bc4ae8fc4cd
David Li <davidli731@gmail.com>
2022-10-09 17:18:51 +1100

    added lerping to bonus pellet

--- PacAssessment/Assets/Scripts/CherryController.cs
+++ PacAssessment/Assets/Scripts/CherryController.cs
@@ -5,23 +5,30 @@ using UnityEngine;
 public class CherryController : MonoBehaviour
 {
     private const float waitUntilSpawn = 10.0f;
-    private const float durationOfTravel = 5.0f;
+    private const float durationOfTravel = 10.0f;
 
-    [SerializeField] private GameObject bonusGO;
+    [SerializeField] private GameObject bonusGOPrefab;
+    [SerializeField] private Transform parent;
 
     private bool spawnBonus = false;
     private Direction directionOfSpawn;
     private Direction directionOfTravel;
     private Vector3 startPos;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Vector3 endPos;
+    private DemoTween bonusTween;
+    private GameObject bonusGO;
 
     // Update is called once per frame
     void Update()
+    {
+        createBonus();
+        handleMovement();
+    }
+
+    /// <summary>
+    /// Create bonus every 10 seconds
+    /// </summary>
+    private void createBonus()
     {
         if (Time.timeSinceLevelLoad % waitUntilSpawn >= 0.0f && !spawnBonus)
         {
@@ -32,23 +39,54 @@ public class CherryController : MonoBehaviour
             {
                 directionOfTravel = Direction.Down;
                 startPos = new Vector3(0.0f, 230.0f, 0.0f);
+                endPos = new Vector3(0.0f, -230.0f, 0.0f);
             }
             else if (directionOfSpawn == Direction.Down)
             {
                 directionOfTravel = Direction.Up;
                 startPos = new Vector3(0.0f, -230.0f, 0.0f);
+                endPos = new Vector3(0.0f, 230.0f, 0.0f);
             }
             else if (directionOfSpawn == Direction.Left)
             {
                 directionOfTravel = Direction.Right;
                 startPos = new Vector3(410.0f, 0.0f, 0.0f);
+                endPos = new Vector3(-410.0f, 0.0f, 0.0f);
             }
             else if (directionOfSpawn == Direction.Right)
             {
                 directionOfTravel = Direction.Left;
                 startPos = new Vector3(-410.0f, 0.0f, 0.0f);
+                endPos = new Vector3(410.0f, 0.0f, 0.0f);
             }
-            //Instantiate(bonusGO, )
+
+            bonusGO = Instantiate(bonusGOPrefab, Vector3.zero, Quaternion.identity, parent);
+            bonusGO.transform.localPosition = startPos;
+
+            if (bonusTween == null)
+            {
+                bonusTween = new DemoTween(bonusGO.transform.localPosition, endPos, Time.timeSinceLevelLoad, durationOfTravel, directionOfTravel);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Handle object movement tweening
+    /// </summary>
+    private void handleMovement()
+    {
+        if (bonusTween != null)
+        {
+            if (bonusGO.transform.localPosition != endPos)
+            {
+                bonusGO.transform.localPosition = Vector3.Lerp(bonusTween.StartPos, bonusTween.DestPos, (Time.timeSinceLevelLoad - bonusTween.StartTime) / durationOfTravel);
+            }
+            else
+            {
+                spawnBonus = false;
+                Destroy(bonusGO);
+                bonusTween = null;
+            }
         }
     }
 }
